3de84cd372c1a0f49eb505885e3deb8f0e3061a4
Luiz André Furlan <lamfsantos5@gmail.com>
2023-01-29 22:37:23 -0400

    Autenticação

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+	def new
+		
+	end
+
+	def create
+		user = User.find_by(username: params[:session][:username].downcase)
+	    if user && user.authenticate(params[:session][:password])
+			sign_in(user)
+			redirect_to user_path(user)
+	    else
+			flash.now[:danger] = "Sorry, we couldn't find an account with this username. Please check you're using the right username and try again"
+			render 'new'
+		end
+    end
+
+    def destroy
+	    sign_out
+	    flash[:success] = 'Logout'
+	    redirect_to login_path
+	end
+end

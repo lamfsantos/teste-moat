4ad3c032e7dec02c3c77af9fd2259c3459d520ef
Luiz André Furlan <lamfsantos5@gmail.com>
2023-01-29 20:44:12 -0400

    estrutura para os partials e as páginas estáticas

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "static_pages#index"
+
+  get 'about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
 end

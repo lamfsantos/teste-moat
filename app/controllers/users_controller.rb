6b301ef3b11ef9a74a34c02480bf5a39ed5220e7
Luiz André Furlan <lamfsantos5@gmail.com>
2023-01-29 22:06:38 -0400

    mensagens de validação adicionadas ao front do cadastro do user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
+	
+
 	def new
 		@user = User.new
 
-		roles_controller = RolesController.new
-		@roles = roles_controller.get_all
+		@roles = get_roles
 	end
 
 	def create
-		
+		@user = User.new(user_params)
+		@roles = get_roles
+
+		if @user.save
+			flash[:success] = 'User registered.'
+			redirect_to root_url
+		else
+			render 'new'
+		end
 	end
+
+	def get_roles
+		roles_controller = RolesController.new
+		roles_controller.get_all
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:full_name, :username, :password, :password_confimration, :role)
+		end
 end

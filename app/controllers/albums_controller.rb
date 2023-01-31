9b496081bedd1e18e47c41001be092a2ec45cc6b
Luiz André Furlan <lamfsantos5@gmail.com>
2023-01-31 11:32:49 -0400

    bug do delete album resolvido

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -73,13 +73,13 @@ class AlbumsController < ApplicationController
   # DELETE /albums/1 or /albums/1.json
   def destroy
     artist = @album.artist
+
     @album.destroy
-
-
+    
     #object being destroyed  but the format is accusing nil
     respond_to do |format|
-      format.html { render(:text => "not implemented") }
-      format.html { redirect_to albums_url, notice: "Album was successfully destroyed." }
+      format.html { redirect_to albums_url(artist), notice: "Album was successfully destroyed." }
+      format.json { head :no_content }
     end
   end
 
